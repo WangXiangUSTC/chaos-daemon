c15c71fe55a424bbcac60436d1aefcd8623f70dd
xiang <xiang13225080@163.com>
2021-06-28 10:55:53 +0800

    test
    Signed-off-by: xiang <xiang13225080@163.com>

--- hack/version.sh
+++ hack/version.sh
@@ -16,7 +16,7 @@
 set -euo pipefail
 
 function chaosd::version::get_version_vars() {
-  echo "GIT_COMMIT: $GIT_COMMIT"
+  # echo "GIT_COMMIT: $GIT_COMMIT"
   if [[ -n ${GIT_COMMIT-} ]] || GIT_COMMIT=$(git rev-parse "HEAD^{commit}" 2>/dev/null); then
     echo "GIT_COMMIT: $GIT_COMMIT"
     # Use git describe to find the version based on tags.
